33a01f7cc4b5bb4e83a96aa7126af710c911d60e
Erik Cikalleshi <61154444+ErikCikalleshi@users.noreply.github.com>
2024-07-29 13:02:04 +0200

    merged results to .csv

--- evaluation_results/main.py
+++ evaluation_results/main.py
@@ -1,11 +1,11 @@
 import csv
 import pandas as pd
 # Read the CSV file exported from the Google Form
-csv_file_path = 'responses.csv'
+csv_file_path = 'Part 2 - Tabellenblatt1.csv'
 responses_df = pd.read_csv(csv_file_path)
 
 # Read the Google Sheet containing the formatted text
-sheet_file_path = 'formatted_text_order.csv'
+sheet_file_path = 'Historical Model Evaluation Survey- Part 2 (Antworten) - Formularantworten 1.csv'
 
 pd_formatted_text = pd.read_csv(sheet_file_path)
 
@@ -18,7 +18,7 @@ for index, row in responses_df.iterrows():
 
 # Write the merged data to a new CSV file
 merged_file_path = 'merged_responses.csv'
-fieldnames = list(responses[0].keys()) + ['Formatted Text']
+fieldnames = list(responses_df[0].keys()) + ['Formatted Text']
 with open(merged_file_path, 'w', newline='', encoding='utf-8') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
