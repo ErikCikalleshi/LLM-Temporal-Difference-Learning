900530fa92bc4eb7096665eeb25d457b420aeb3b
Erik Cikalleshi <61154444+ErikCikalleshi@users.noreply.github.com>
2024-07-29 13:02:04 +0200

    merged results to .csv

--- evaluation_results/main2.py
+++ evaluation_results/main2.py
@@ -1,8 +1,9 @@
+import numpy
 import pandas as pd
 
 # Read CSV files into pandas DataFrames
-formatted_text_df = pd.read_csv('Formatted Text Order - Tabellenblatt1.csv')
-responses_df = pd.read_csv('Historical Model Evaluation Survey (Antworten) - Formularantworten 1.csv')
+formatted_text_df = pd.read_csv('Part 1 - Tabellenblatt1.csv')
+responses_df = pd.read_csv('Historical Model Evaluation Survey - Part 1 (Antworten) - Formularantworten 1.csv')
 
 # Initialize an empty list to store the split rows
 split_rows = []
@@ -13,14 +14,19 @@ for _, row in responses_df.iterrows():
     # Iterate over each pair of Historical Informativeness and Truthfulness
     # skip the first 3 rows because they are not relevant
     print(int(section_number))
-    row = row[3:]
-    for i in range(1, len(row)):
+    row = row[2:]
+    count_responses = 0
+    for i in range(0, len(row), 2):
         info = row[i]
-        # truth = row[i + 1]
-        # print({'sectionNumber': int(section_number), 'Historical Informativeness': info})
+        fluency = row[i + 1]
+        print({'sectionNumber': int(section_number), 'Historical Informativeness': info, 'Historical Fluency': fluency})
         split_rows.append({'sectionNumber': int(section_number),
-                           'Historical Informativeness': info})
-        # 'Historical Truthfulness': truth})
+                           'Historical Informativeness': info,
+                           'Historical Fluency': fluency})
+        if not pd.isna(info) or not pd.isna(fluency):
+            count_responses += 1
+    if count_responses < 12:
+        print('Error: Expected 12 responses for section', section_number, 'but found', count_responses)
 
 split_rows_df = pd.DataFrame(split_rows)
 
@@ -30,23 +36,23 @@ print(split_rows_df)
 split_rows_df = split_rows_df.dropna()
 
 # Ensure the column 'Historical Informativeness' exists in formatted_text_df
-if 'Historical Informativeness' not in formatted_text_df.columns:
+if 'Historical Informativeness' not in formatted_text_df.columns and 'Historical Fluency' not in formatted_text_df.columns:
     formatted_text_df['Historical Informativeness'] = None
+    formatted_text_df['Historical Fluency'] = None
 
 for section_number in split_rows_df['sectionNumber'].unique():
     # get historical informativeness for each section
-    section_data = split_rows_df[split_rows_df['sectionNumber'] == int(section_number)][
+    section_data_hist_info = split_rows_df[split_rows_df['sectionNumber'] == int(section_number)][
         'Historical Informativeness'].values
-
-    # Ensure the lengths match by repeating or trimming the data as necessary
-    formatted_text_len = formatted_text_df[formatted_text_df['sectionNumber'] == int(section_number)].shape[0]
-    if len(section_data) > formatted_text_len:
-        section_data = section_data[:formatted_text_len]
-    elif len(section_data) < formatted_text_len:
-        section_data = list(section_data) + [None] * (formatted_text_len - len(section_data))
+    section_data_hist_fluency = split_rows_df[split_rows_df['sectionNumber'] == int(section_number)][
+        'Historical Fluency'].values
 
     formatted_text_df.loc[
-        formatted_text_df['sectionNumber'] == int(section_number), 'Historical Informativeness'] = section_data
+        formatted_text_df['sectionNumber'] == int(
+            section_number), 'Historical Informativeness'] = section_data_hist_info.astype(int)
+    formatted_text_df.loc[
+        formatted_text_df['sectionNumber'] == int(
+            section_number), 'Historical Fluency'] = section_data_hist_fluency.astype(int)
 
 print(formatted_text_df)
 # Save the merged DataFrame to a new CSV file
